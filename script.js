c3513696e96a38f55d539ccf2cba78899cd17b51
Josue <jdd1993@hotmail.com>
2022-10-18 21:43:04 -0400

    Add functionality to selection button.

--- script.js
+++ script.js
@@ -4,43 +4,27 @@
 const body = document.querySelector('body');
 const container = document.querySelector('div[id=container]');
 container.classList.add('container');
-let columns = 16;
+let columns = 0;
 let rows = columns;
 const button = document.querySelector('button');
 
-/*
+
 function popUp(){
 
     let election = prompt('Enter the amount of squares per side for the new grid', 16);
 
-    
-
-   if (election != 16){
-
     columns = election;
-
-    console.log(columns)
-
-   }
-
-}
-
-button.addEventListener('click', popUp);
-
-*/
-
-
-if (columns == 16) {
+    rows = columns;
 
     for (let i = 0; i < columns; i++){
 
-    
+        
         const col = document.createElement('div');
         col.classList.add('col');
         container.appendChild(col);
         col.style.width = `${columns}px`;
-    
-    
+        
+        
         for(let j = 0; j < rows; j++){
     
             const row = document.createElement('div');
@@ -53,22 +37,10 @@ if (columns == 16) {
                 row.style.background = "black";
             }
             
-            row.addEventListener('mouseover', colorTrail) 
+            row.addEventListener('mouseover', colorTrail); 
     
         }
-        
-        
     }
-
 }
 
-
-
-
-
-
-
-
-
-
-
+button.addEventListener('click', popUp);
