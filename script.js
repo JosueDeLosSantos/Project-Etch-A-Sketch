994f8eac2d3ca6480e2692e0469c62419071c280
Josue <jdd1993@hotmail.com>
2022-10-18 22:23:19 -0400

    complete project

--- script.js
+++ script.js
@@ -6,7 +6,8 @@ const container = document.querySelector('div[id=container]');
 container.classList.add('container');
 let columns = 0;
 let rows = columns;
-const button = document.querySelector('button');
+const button = document.querySelector('#pixels');
+const button2 = document.querySelector('#reload');
 
 
 function popUp(){
@@ -16,31 +17,48 @@ function popUp(){
     columns = election;
     rows = columns;
 
-    for (let i = 0; i < columns; i++){
+    if(election <= 100){
+
+        for (let i = 0; i < columns; i++){
 
         
-        const col = document.createElement('div');
-        col.classList.add('col');
-        container.appendChild(col);
-        col.style.width = `${columns}px`;
-        
-        
-        for(let j = 0; j < rows; j++){
-    
-            const row = document.createElement('div');
-            row.classList.add('row');
-            col.appendChild(row);
-            row.style.height = `${columns}px`;
-    
-    
-            function colorTrail() {
-                row.style.background = "black";
-            }
+            const col = document.createElement('div');
+            col.classList.add('col');
+            container.appendChild(col);
+            col.style.width = `${columns}px`;
             
-            row.addEventListener('mouseover', colorTrail); 
-    
+            
+            for(let j = 0; j < rows; j++){
+        
+                const row = document.createElement('div');
+                row.classList.add('row');
+                col.appendChild(row);
+                row.style.height = `${columns}px`;
+        
+        
+                function colorTrail() {
+                    row.style.background = "black";
+                }
+                
+                row.addEventListener('mouseover', colorTrail); 
+        
+            }
         }
+
+    } else {
+        alert("You can't select more than 100 squares.")
+        window.location.reload()
+        return false;
     }
 }
 
-button.addEventListener('click', popUp);
+button.addEventListener('click', popUp,{
+    once: true
+});
+
+function refresher(){
+    window.location.reload()
+    return false;
+}
+
+button2.addEventListener('click', refresher);
